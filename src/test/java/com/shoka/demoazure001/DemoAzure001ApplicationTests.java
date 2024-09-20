ec1e540e4b7ca6c23809f1c0d4f0934553998203
Arnold Mollo <arnold.mollo@dimexa.com.pe>
2024-09-20 17:44:56 -0500

    first commit

---
+++ src/test/java/com/shoka/demoazure001/DemoAzure001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoka.demoazure001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAzure001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
